546ee04435dd8705bdd069c45d7af66221e95840
MayurCodeJet <shelkemayuresh8@gmail.com>
2022-03-14 12:42:28 +0530

    main

---
+++ casting variable.py
@@ -0,0 +1,9 @@
+# so we have to cast the variable to diffrent it between in the other caterogry 
+# so there are 3 types of the casting of the variable 
+a = int(2) # this is int which is called as the integer casting which indicate the number   
+b = str(3) # this is str which is called as the string in the variable 
+c = float(3) # this is float variable which is show the varibale in the real number 
+ 
+print(a)
+print(b)
+print(c)
---
+++ type variable.py
@@ -0,0 +1,16 @@
+from re import T, X
+
+
+m = 7 # m is the interger 
+y = "mayuresh"  # y is the string 
+print(type(m)) #type m will show the that the m is string or int 
+print(type(y)) #type y will show th that y is the string or int  
+
+
+# now we will the single or double quotes 
+x = "John" 
+X = "John" #new variable element are created here 
+print(x)
+print(X)
+# now tell me are they both same ?
+# yes they both are the same because they are just quotes 
