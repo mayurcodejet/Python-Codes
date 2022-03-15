12a9a710056675a2bc3687944bad208822e03355
MayurCodeJet <shelkemayuresh8@gmail.com>
2022-03-15 22:17:12 +0530

    main

---
+++ Number/complex.py
@@ -0,0 +1,11 @@
+#complex number are written with the "j" as the imaginary part
+
+#example of the complex number
+x = 19+1j
+y = 20j
+z =  -21j
+
+#print the value
+print(type(x))
+print(type(y))
+print(type(z))
