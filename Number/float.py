ef21260b9be5f18e4d04486dd7178f85c4b05a5b
MayurCodeJet <shelkemayuresh8@gmail.com>
2022-03-15 22:12:20 +0530

    main

--- Number/float.py
+++ Number/float.py
@@ -1,3 +1,14 @@
 #float or the "floating number types" is a number, positive or negative, containing one or more decimals. 
 
 #so here is the simple example for the "float method" 
+#first variable is "a"
+
+x = 20.9 
+#add the variable as you want 
+y =  9.0 
+z = -89.12
+
+#print the value the 
+print(type(x))
+print(type(y))
+print(type(z))
