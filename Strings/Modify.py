4b5ba6b883a86f8f1b7c6d30fd9b806cb9ffe5e2
MayurCodeJet <shelkemayuresh8@gmail.com>
2022-03-17 14:33:45 +0530

    main

---
+++ Strings/Modify.py
@@ -0,0 +1,26 @@
+#built-in methods that can be used on string
+
+#USER CASE
+#a user case method return the string in upper case: 
+a = "Hello Modify Method" 
+print(a.upper())
+
+#lower case
+#b user case method which return thr string to the lower case
+b = "I AWAIT FOR THE DEATH"
+print(b.lower())
+
+#Whitespace remover
+#c user case method in which the whitespace will removed from the beginning to end: 
+c = "Hello, World"
+print(c.strip())
+
+#replace 
+#d user case will replace the string to another string
+a = "Mayuresh Shelke"
+print(a.replace("M","S"))
+
+#split 
+#the split method spilts the string into substrings if it instance the separtaor: 
+d = "lets, seperate" 
+print(d.split(","))
---
+++ Strings/Slicing.py
@@ -0,0 +1,17 @@
+#slice syntax are used to return a range of character 
+#specify the strart index & the index, seperated by a colon ":" to return a part of the string 
+
+a = "hello slicing" 
+print(a[2:5])
+
+#slice from the Start
+#by leaving out the start index, the range will start at the first character: 
+
+b = "Lets start"
+print(b[:5])
+
+#by leaving out the end index, the range will go to the end: 
+#get the characteristics from 2 position 
+
+c = "Heyya Guys"
+print(c[2:1])
